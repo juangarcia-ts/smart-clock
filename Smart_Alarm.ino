cbc83e8535fd5ef7308d13e505c9889fd3545d97
Juan Garcia <juangarcia.ts@gmail.com>
2019-06-30 03:36:24 -0300

    Disparo do alarme

--- Smart_Alarm.ino
+++ Smart_Alarm.ino
@@ -5,122 +5,122 @@
 #include "RTClib.h"
 
 //Constantes
-#define BUZZER            17
-#define POWER_LED         16
 #define TX_PORT           14
 #define RX_PORT           15
-//#define TOUCH_BUTTON    18
+#define BUZZER            17
+#define POWER_LED         16
 #define ALARM_LED         19
 #define NOTA_BUZINA       262
 #define INTERVALO_BUZINA  2000
+#define RTC_DELAY         4
 
 //Variáveis
 SoftwareSerial BTserial(RX_PORT, TX_PORT);
 SevSeg sevseg;
-RTC_DS3231 rtc;  
-String command = "";
-//DateTime alarm = new DateTime();
-bool alarm = true;
+RTC_DS3231 rtc;
+bool hasAlarm = false;
+bool isActive = false;
+String alarmTime;
 int pin = 0;
 int userPin = 0;
 
 void setup() {
   pinMode(POWER_LED, OUTPUT);
-  pinMode(ALARM_LED, OUTPUT);
-  
-  Serial.begin(9600);   
-  Serial.print("Sketch:   ");   
+
+  Serial.begin(9600);
+  Serial.print("Sketch:   ");
   Serial.println(__FILE__);
-  Serial.print("Uploaded: ");   
-  Serial.println(__DATE__);     
+  Serial.print("Uploaded: ");
+  Serial.println(__DATE__);
 
   BTserial.begin(9600);
- 
+
+  randomSeed(9000);
+
   if (!rtc.begin()) {
     Serial.println("DS3231 não encontrado");
     while (1);
-  }   
+  }
 
-  byte hardwareConfig = COMMON_CATHODE; 
+  byte hardwareConfig = COMMON_CATHODE;
   byte numDigits = 4;
   byte digitPins[] = {9, 10, 11, 12};
   byte segmentPins[] = {2, 3, 4, 5, 6, 7, 8};
-  bool resistorsOnSegments = true; 
+  bool resistorsOnSegments = true;
   bool updateWithDelays = false;
-  bool leadingZeros = true; 
+  bool leadingZeros = true;
   bool disableDecPoint = true;
- 
+
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments, updateWithDelays, leadingZeros, disableDecPoint);
   sevseg.setBrightness(90);
 }
 
-void loop() {    
-  digitalWrite(POWER_LED, HIGH); 
-  
-  initializeBluetooth();  
-  
-  DateTime rctNow = rtc.now();
-
-  if (alarm != true) {    
+void loop() {
+  DateTime rtcNow = rtc.now();
+  String strNow = String(rtcNow.hour()) + String(rtcNow.minute() - RTC_DELAY);
+      
+  if (isActive || (hasAlarm == true && alarmTime.equals(strNow))) {
+    BTserial.write("ALARM_ON\r\n");     
+    digitalWrite(POWER_LED, HIGH); 
     displayPIN();
     ringAlarm();
-
-    if (pin == userPin) {
-      alarm = false;      
-    }
   } else {
-    displayTime(rctNow);
-  }     
-} 
-
-void initializeBluetooth() {
-  if (BTserial.available() > 0) 
-  {  
-    while(BTserial.available()) 
-    { 
-      command += (char)BTserial.read();  
-    }  
-     
-    readCommand(command); 
-  }  
-  
-  if (Serial.available())
-  {  
-    delay(10);  
-    Serial.write(Serial.read());  
-    BTserial.write(Serial.read());      
-  } 
-}
-
-void readCommand(String command) {
-  Serial.print(command);
-  
-  if (command.indexOf("SET_ALARM:") {
-    Serial.write("1");
-  } else if (command.indexOf("DISMISS_ALARM:") {
-    Serial.write("2");
+    displayTime(rtcNow);
   }
 
-  command = "";
+  startBluetooth();
+}
+
+void startBluetooth() {
+  String command;
+  
+  if (BTserial.available()) {
+    while (BTserial.available()) {
+      command += String(char(BTserial.read()));
+    }
+
+    if (command.indexOf("SET:") != -1) {
+      alarmTime = command.substring(4, command.length());
+      hasAlarm = true;    
+    } 
+
+    if (command.indexOf("PIN:") != -1) {
+      userPin = command.substring(4, command.length()).toInt();
+
+      if (pin != userPin) {
+        BTserial.write("WRONG_PIN\r\n"); 
+      } else {
+        hasAlarm = false;
+        isActive = false;
+        BTserial.write("ALARM_OFF\r\n"); 
+      }
+    }
+  }
+
+  while (Serial.available()) {
+    delay(10);
+    command = Serial.read();
+    BTserial.print(command);
+  }
 }
 
 void displayTime(DateTime rtcNow) {
-  int currentTime = (rtcNow.hour() * 100) + rtcNow.minute();  
+  int currentTime = (rtcNow.hour() * 100) + rtcNow.minute() - RTC_DELAY;
 
   sevseg.setNumber(currentTime, 2);
-  sevseg.refreshDisplay(); 
+  sevseg.refreshDisplay();
 }
 
 void displayPIN() {
   if (pin == 0) {
-    pin = random(1,9999);
+    pin = random(1, 9999);
   }
-    
+
   sevseg.setNumber(pin, 0);
-  sevseg.refreshDisplay(); 
+  sevseg.refreshDisplay();
 }
 
 void ringAlarm() {
-  tone(BUZZER, NOTA_BUZINA, INTERVALO_BUZINA);  
+  tone(BUZZER, NOTA_BUZINA, INTERVALO_BUZINA);
   noTone(BUZZER);
 }
